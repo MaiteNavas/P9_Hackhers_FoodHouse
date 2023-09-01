344a280ee0c14482199c15a75c6a2b27777692f1
lauuloulu <lprsa18@gmail.com>
2023-09-01 14:06:31 +0200

    errors

--- database/migrations/2023_09_01_085418_create_categoria_table.php
+++ database/migrations/2023_09_01_085418_create_categoria_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categoria', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_categoria');
             $table->timestamps();
         });
     }
