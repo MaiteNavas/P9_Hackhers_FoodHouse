ca1276d9c38014190e01ccc9374107ca1bb47d22
lauuloulu <lprsa18@gmail.com>
2023-09-03 14:42:25 +0200

    controller usuario: inicio CRUD

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Usuario;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class UsuarioController extends Controller
 {
-    public function index()
+    public function index_usuario(){
+        $usuarios= Usuario::all($columns = ['*']);
+
+        return view('admin.usuario.index_usuario',compact('usuarios'));
+    }
 
 }
---
+++ resources/views/admin/index_admin.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app2')
+
+@section('content')
+
+
+    <div class="center">
+        <div class="botones">
+            <a class="btn custom-btn" href="{{ route('usuario.index')}}">Usuarios</a>
+            <a class="btn custom-btn" href="{{ route('categoria.index')}}">Categorias</a>
+            <a class="btn custom-btn" href="">Productos</a>
+            <a class="btn custom-btn" href="">Ventas</a>
+        </div>
+    </div>
+    @endsection('content')
+    </body>
+</html>
