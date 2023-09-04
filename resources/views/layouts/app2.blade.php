52186a51da6adca625347c66e590df412b01cd3a
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-09-04 09:53:40 +0200

    boton atrás navbar

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -11,6 +11,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
    
     </head>
+
+    <header>
+        <a href="javascript:history.back()"> Volver Atrás</a>
+    </header>
     
 
     <body>
