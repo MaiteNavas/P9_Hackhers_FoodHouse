e4e03cb540a0fc2754501adc5d846c49858de436
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-09-06 09:07:14 +0200

    C:/Program Files/Git/Cart no funciona

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Darryldecode\Cart\CartServiceProvider::class
     ])->toArray(),
 
     /*
@@ -182,6 +183,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
