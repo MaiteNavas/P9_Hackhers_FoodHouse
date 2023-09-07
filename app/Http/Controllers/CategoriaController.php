b2a885f959bec46b002cf3c49a708ecef892b61c
Maggie <maggie.filgueira@gmail.com>
2023-09-07 12:49:18 +0200

    ASW S3

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -7,6 +7,9 @@ use App\Models\Categoria;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\AwsS3V3\PortableVisibilityConverter;
+
 
 class CategoriaController extends Controller
 {
@@ -18,17 +21,19 @@ class CategoriaController extends Controller
     public function create_categoria(){
         return view('admin.categoria.create_categoria');
     }
-    public function store_categoria(Request $request){
-        Categoria::create([
-            'id_categoria' => $request->id_categoria,
-            'nombre_categoria' => $request->nombre_categoria,
-        ]);
-        return redirect()->route('categoria.index');
-    }
-    public function edit_categoria(Categoria $categoria){
+    // public function store_categoria(Request $request){
+    //     Categoria::create([
+    //         'id_categoria' => $request->id_categoria,
+    //         'nombre_categoria' => $request->nombre_categoria,
+    //     ]);
+    //     return redirect()->route('categoria.index');
+    // }
+    // public function edit_categoria(Categoria $categoria){
         
-        return view('admin.categoria.edit_categoria',compact('categoria'));
-    }
+    //     return view('admin.categoria.edit_categoria',compact('categoria'));
+    // }
+
+    
     public function update_categoria(Request $request,Categoria $categoria){
         $categoria->update([
             'id_categoria' => $request->id_categoria,
@@ -42,5 +47,32 @@ class CategoriaController extends Controller
         return redirect()->route('categoria.index');
     }
 
-}
 
+//esto es una prueba
+
+
+// public function store_categoria(Request $request)
+// {
+//     $request->validate([
+//         'nombre_categoria' => 'required|string|max:255',
+//         'imagen_categoria' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Ajusta las reglas de validación según tus necesidades
+//     ]);
+
+//     // Subir la imagen a AWS S3 y obtener su URL
+//     if ($request->hasFile('imagen_categoria')) {
+//         $imagenPath = $request->file('imagen_categoria')->store('categorias', 's3'); // 'categorias' es la carpeta en S3 donde se almacenará la imagen
+//         $imagenUrl = Storage::disk('s3')->url($imagenPath);
+//     } else {
+//         $imagenUrl = null; // No se cargó ninguna imagen
+//     }
+
+//     // Crear la categoría en la base de datos
+//     Categoria::create([
+//         'nombre' => $request->input('nombre_categoria'),
+//         'imagen_url' => $imagenUrl,
+//     ]);
+
+//     return redirect()->route('categoria.index')->with('success', 'Categoría creada con éxito.');
+// }
+// 
+}
--- resources/views/admin/categoria/create_categoria.blade.php
+++ resources/views/admin/categoria/create_categoria.blade.php
@@ -4,14 +4,18 @@
 <a class="btn custom-btn" href="{{ route('categoria.index')}}">Volver</a>
 
 <div class="container-with-border bg-light" >
-        <h1>Crear Categoria</h1>
-        <form action="{{ route('categoria.store')}}" method="POST" autocomplete="off">
-            @csrf
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Nombre de la categoria</label>
-                <input type="text" name="nombre_categoria" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <button type="submit" class="btn custom-btn">Guardar</button>
-
-        </form>
-    </div>
+    <h1>Crear Categoria</h1>
+    <form action="{{ route('categoria.store')}}" method="POST" autocomplete="off" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nombre de la categoria</label>
+            <input type="text" name="nombre_categoria" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="imagen_categoria" class="form-label">Imagen de la categoria</label>
+            <input type="file" name="imagen_categoria" class="form-control" id="imagen_categoria">
+        </div>
+        <button type="submit" class="btn custom-btn">Guardar</button>
+    </form>
+</div>
 @endsection
