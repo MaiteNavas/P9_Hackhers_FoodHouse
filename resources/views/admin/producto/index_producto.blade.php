e665599239c1df28269764fdf7f078d01975f822
Maggie <maggie.filgueira@gmail.com>
2023-09-10 17:49:18 +0200

    fotos card

--- resources/views/admin/producto/index_producto.blade.php
+++ resources/views/admin/producto/index_producto.blade.php
@@ -5,25 +5,21 @@
     <h1>LISTA DE PRODUCTOS</h1>
     <div class="row" id= "product-container">
     @forelse($productos as $producto)
-    <div class="col-md-3 col-6 mb-4 product d-flex">
-    <div class="card w-100 d-flex flex-fill">
-    <i class="align-self-start">{{ $producto->categoria->nombre_categoria }}</i>
-        <div class="embed-responsive embed-responsive-1by1">
-            <img class="embed-responsive-item img-fluid producto-imagen h-100" src="{{ $producto->ruta_foto_producto }}" alt="Foto del Producto">
-        </div>
+    <div class="card" style="width: 18rem;">
+        <i class="align-self-start">{{ $producto->categoria->nombre_categoria }}</i>
+        <img src="{{ $producto->ruta_foto_producto }}" class="card-img-top" alt="Foto del Producto">
         <div class="card-body">
             <h4 class="card-title">{{ $producto->nombre_producto }}</h4>
-            <p>{{ $producto->descripcion }}</p>
+            <h6 class="card-title">{{ $producto->categoria->nombre_categoria }}</h6>
             <p class="card-text"><strong>Precio: </strong> {{ $producto->precio }} €</p>
+            <p class="card-text">{{ $producto->descripcion }}</p>
             <form action="{{ route('producto.delete', $producto->id_producto) }}" method="POST">
                 @csrf
-                @method("DELETE")
-                <button type="submit" class="btn custom-btn">Eliminar</button>
-            </form>
+                    @method("DELETE")
+                        <button type="submit" class="btn btn-primary">Eliminar</button>
+            </form> 
         </div>
     </div>
-</div>
-
 
         @empty
         <p>No hay productos registrados</p>
--- resources/views/usuario/products.blade.php
+++ resources/views/usuario/products.blade.php
@@ -1,8 +1,12 @@
 @extends('usuario.shop')
-   
 @section('content')
-
-
+<div class="input-group mb-3">
+    <input type="text" id="searchInput" class="form-control" placeholder="Buscar por plato, ingrediente...">
+    <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button" id="searchButton">Buscar</button>
+        <button class="btn btn-outline-secondary" type="button" id="clearSearchButton">Limpiar búsqueda</button>
+    </div>
+</div>
 <div class="filter-container">
     <label for="inputPassword" class="col-sm-2 col-form-label">Filtrar por categoría:</label>
     <select id="rolFilter" class="form-select">
@@ -13,38 +17,30 @@
     </select>
 </div>
 <br>
-
 <div class="row" id= "product-container">
     @foreach($products as $product)
-        <div class="col-md-3 col-6 mb-4 product" data-categoria="{{ $product->categoria->id_categoria }}">
-            <div class="card">
-                <div class="card-body">
-                    <i>{{ $product->categoria->nombre_categoria }}</i>
-                    <h4 class="card-title">{{ $product->nombre_producto }}</h4>
-                    <p>{{ $product->descripcion }}</p>
-                    <input id= "categorias" value= "{{ $product->categoria->id_categoria }}" hidden></input>
-                    <p class="card-text"><strong>Precio: </strong> {{ $product->precio }} €</p>
-                    
-                    <p class="btn-holder"><a href="{{ route('addProduct.to.cart', $product->id_producto) }}" class="btn btn-outline-success">Añadir al pedido</a> </p>
-                </div>
-            </div>
+    <div class="card" style="width: 18rem;">
+        <i class="align-self-start">{{ $product->categoria->nombre_categoria }}</i>
+        <img src="{{ $product->ruta_foto_producto }}" class="card-img-top" alt="Foto del Producto">
+        <div class="card-body">
+            <h4 class="card-title">{{ $product->nombre_producto }}</h4>
+            <h6 class="card-title">{{ $product->categoria->nombre_categoria }}</h6>
+            <p class="card-text"><strong>Precio: </strong> {{ $product->precio }} €</p>
+            <p class="card-text">{{ $product->descripcion }}</p>
+            <p class="btn-holder"><a href="{{ route('addProduct.to.cart', $product->id_producto) }}" class="btn btn-outline-success">Añadir al pedido</a> </p>
         </div>
+    </div>
     @endforeach
 </div>
-    
 @endsection
-
 @section('scripts')
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
 $(document).ready(function () {
     $('#rolFilter').on('change', function () {
         var selectedCategoria = $(this).val();
-
         // Oculta todos los productos
         $('.product').hide();
-
         if (selectedCategoria === 'all') {
             // Si se selecciona "Todas", muestra todos los productos
             $('.product').show();
@@ -53,10 +49,41 @@ $(document).ready(function () {
             $('.product[data-categoria="' + selectedCategoria + '"]').show();
         }
     });
+      // Función para realizar la búsqueda
+      function performSearch() {
+        var searchTerm = $('#searchInput').val().toLowerCase();
+        // Oculta todos los productos
+        $('.product').hide();
+        if (searchTerm === '') {
+            // Si la barra de búsqueda está vacía, muestra todos los productos
+            $('.product').show();
+        } else {
+            // Muestra solo los productos que contienen el término de búsqueda en su nombre o descripción
+            $('.product').each(function () {
+                var productName = $(this).find('.card-title').text().toLowerCase();
+                var productDescription = $(this).find('p').text().toLowerCase(); // Obtener la descripción
+                if (productName.includes(searchTerm) || productDescription.includes(searchTerm)) {
+                    $(this).show();
+                }
+            });
+        }
+    }
+    // Agrega el evento para buscar al hacer clic en el botón
+    $('#searchButton').on('click', performSearch);
+    // Agrega el evento para buscar al presionar Enter en el campo de búsqueda
+    $('#searchInput').on('keypress', function (e) {
+        if (e.key === 'Enter') {
+            performSearch();
+        }
+    });
+    // Agrega el evento para limpiar la búsqueda
+    $('#clearSearchButton').on('click', function () {
+        // Limpiar el campo de búsqueda
+        $('#searchInput').val('');
+        // Mostrar todos los productos nuevamente
+        $('.product').show();
+    });
 });
 </script>
-
-
 @endsection
-
 </body>
