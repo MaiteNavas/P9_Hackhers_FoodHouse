477117d685a8e1d181acb92b5052c0040182f687
MaiteNavas <maitenavas626@gmail.com>
2023-09-10 11:05:36 +0200

    tabla ventas

--- resources/views/admin/ventas/ventas_index.blade.php
+++ resources/views/admin/ventas/ventas_index.blade.php
@@ -8,29 +8,22 @@
             <th>ID PEDIDO</th>
             <th>PRECIO</th>
             <th>FECHA</th>
-
         </tr>
     </thead>
     <tbody> 
     @php $totalPedido = 0 @endphp
-    @foreach($pedidos as $pedido) 
-    @php $totalPedido += $pedido->precio_pedido @endphp
-         
-                <tr rowId="{{ $pedido->id_pedido }}">
-                    <td data-th="Price">{{ $pedido->id_pedido }} €</td>
-                    <td data-th="Price">{{ $pedido->precio_pedido }} €</td>
-                    
-                </tr>
-
-                @endforeach
+        @foreach($pedidos as $pedido) 
+            @php $totalPedido += $pedido->precio_pedido @endphp
+        <tr rowId="{{ $pedido->id_pedido }}">
+            <td data-th="Price">{{ $pedido->id_pedido }} €</td>
+            <td data-th="Price">{{ $pedido->precio_pedido }} €</td>          
+        </tr>
+        @endforeach
     </tbody>
     <tfoot>
-       <tr>
-        
-        <td colspan="5" class="text-right"><h5><strong>Total</strong> {{ $totalPedido }} € </h5>
-        </td>
+        <tr>
+            <td colspan="5" class="text-right"><h5><strong>Total</strong> {{ $totalPedido }} € </h5></td>
         </tr>
-        <input type="hidden" name="precio_pedido" value="{{ $totalPedido }}">
     </tfoot>
 </table>
 
